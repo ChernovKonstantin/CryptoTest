0faa086f4cc827958de5c60e64fa06140905e710
ChernovKonstantin <chernov.konstantin@gmail.com>
2025-05-21 16:12:40 +0200

    Initial Commit

---
+++ CryptoTest/CryptoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTestApp.swift
+//  CryptoTest
+//
+//  Created by Chernov Kostiantyn on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTestTests/CryptoTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTestTests.swift
+//  CryptoTestTests
+//
+//  Created by Chernov Kostiantyn on 21.05.2025.
+//
+
+import Testing
+@testable import CryptoTest
+
+struct CryptoTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
